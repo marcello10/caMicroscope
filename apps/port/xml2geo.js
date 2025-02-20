7641106a038ebd2f179668c81b0a93e7e4e6aaa9
Birm <birm@rbirm.us>
2025-02-20 13:54:00 -0500

    need the polygon not range form for bounding box

--- apps/port/xml2geo.js
+++ apps/port/xml2geo.js
@@ -81,7 +81,7 @@ function xml2geo() {
       },
       'bound': {
         'type': 'BoundingBox',
-        'coordinates': [[minX, minY], [maxX, maxY]],
+        'coordinates': [boundRect],
       },
     };
 
